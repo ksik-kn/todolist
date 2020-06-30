6ccec37ff1efc74bf24eddde386b1b161dffef1a
ksik-kn <ksik0402kn1055@gmail.com>
2020-06-30 23:08:20 +0900

    欲しいものリスト追加(練習用)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,36 +2,24 @@
   <div>
     <h1>{{ msg }}</h1>
     <counter />
+    <item-list />
   </div>
 </template>
 
 <script>
 import Counter from "./Counter.vue";
+import ItemList from "./ItemList.vue";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String
   },
   components: {
-    Counter
+    Counter,
+    ItemList
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style></style>
