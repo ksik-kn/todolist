20309505a39006881684b7d0dac150f436e8ee57
ksik-kn <ksik0402kn1055@gmail.com>
2020-07-01 14:40:35 +0900

    カウンター　アイテムリストの削除

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,18 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-    <counter />
-    <item-list />
     <to-do-list />
   </div>
 </template>
 
 <script>
-import Counter from "./Counter.vue";
-import ItemList from "./ItemList.vue";
 import ToDoList from "./ToDoList.vue";
-
 export default {
   name: "HelloWorld",
   props: {
     msg: String
   },
   components: {
-    Counter,
-    ItemList,
     ToDoList
   }
 };
